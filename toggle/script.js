d4958cd7af27ee86f4913a23fdbbf6cc95dc785f
Shivansh Mishra <shivnsh01@gmail.com>
2025-07-20 20:37:29 +0530

    20-07-2025 Update

---
+++ toggle/script.js
@@ -0,0 +1,6 @@
+const buttonElement = document.querySelector(".SHIVANSH");
+buttonElement.addEventListener("click", function togglePvalueOnClick(){
+    console.log("Vijaya is the best teacher");
+});
+
+console.log("buttonElement",buttonElement);
